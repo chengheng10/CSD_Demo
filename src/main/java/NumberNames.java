ca44978dc9a92e432b8c6316044b3077fcc105d3
chengheng10 <chengheng10@163.com>
2018-06-17 10:39:56 +0800

    number names hundred

--- src/main/java/NumberNames.java
+++ src/main/java/NumberNames.java
@@ -15,8 +15,39 @@ Spell out a number. For example
 [Source http://rosettacode.org]
    */
 
+
+import java.util.HashMap;
+
 public class NumberNames {
-    public static String getNumberNames(int i) {
-        return "ninety nine";
+    public HashMap<String, String> numberHash = new HashMap<String, String>();
+
+    public NumberNames() {
+        this.numberHash.put("0", "");
+        this.numberHash.put("1", "one");
+        this.numberHash.put("2", "two");
+        this.numberHash.put("3", "three");
+        this.numberHash.put("4", "four");
+        this.numberHash.put("5", "five");
+        this.numberHash.put("6", "six");
+        this.numberHash.put("7", "seven");
+        this.numberHash.put("8", "eitht");
+        this.numberHash.put("9", "nine");
+        this.numberHash.put("10", "ten");
+        this.numberHash.put("11", "eleven");
+        this.numberHash.put("12", "twelve");
+    }
+
+    public String getNumberNames(long number) {
+        String result = "";
+        if (number < 12) {
+            result = this.numberHash.get(String.valueOf(number));
+        } else if (number < 100) {
+            result = this.numberHash.get(String.valueOf(number / 10)) + "ty " + this.numberHash.get(String.valueOf(number % 10));
+        } else if (number >= 100) {
+            result = this.numberHash.get(String.valueOf(number / 100)) + " hundred" + ((number%100==0)?"":" and ")+getNumberNames(number % 100);
+        }
+
+
+        return result;
     }
 }
--- src/test/java/NumberNamesTest.java
+++ src/test/java/NumberNamesTest.java
@@ -1,11 +1,24 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class NumberNamesTest {
-    @Test
-    public void numberNamesTestCase001() {
-        Assert.assertEquals("ninety nine",NumberNames.getNumberNames(99));
+    NumberNames numberNames;
+    @Before
+    public void setUp(){
+         numberNames = new NumberNames();
     }
 
-
+    @Test
+    public void numberNamesTestCase001() {
+        Assert.assertEquals("ninety nine",numberNames.getNumberNames(99));
+    }
+    @Test
+    public void numberNamesTestCase002() {
+        Assert.assertEquals("three hundred",numberNames.getNumberNames(300));
+    }
+    @Test
+    public void numberNamesTestCase003() {
+        Assert.assertEquals("three hundred and ten",numberNames.getNumberNames(310));
+    }
 }
