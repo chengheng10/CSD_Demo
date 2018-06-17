8e155784acb980efe87c9cf26b5e8c631cb9741d
chengheng10 <chengheng10@163.com>
2018-06-17 15:13:29 +0800

    Roman Numeral

--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -6,4 +6,8 @@ public class RomanNumeralsTest {
     public void romanNummeralsTestCase001(){
         Assert.assertEquals("I",RomanNumerals.romanNumeralRepresentation(1));
     }
+    @Test
+    public void romanNummeralsTestCase002(){
+        Assert.assertEquals("II",RomanNumerals.romanNumeralRepresentation(2));
+    }
 }
