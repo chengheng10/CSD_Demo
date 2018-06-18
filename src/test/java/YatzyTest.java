95702391074b69bded44113d5ff4cf36175e11b9
chengheng10 <chengheng10@163.com>
2018-06-18 11:01:13 +0800

    Yztzy testCase002

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -7,5 +7,8 @@ public class YatzyTest {
     public void yatzyTestCase001(){
         Assert.assertEquals(14,Yatzy.getYatzyResult(1,1,3,3,6));
     }
-
+    @Test
+    public void yatzyTestCase002(){
+        Assert.assertEquals(21,Yatzy.getYatzyResult(4,5,5,6,1));
+    }
 }
