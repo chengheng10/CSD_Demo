ed228e7c5e7577381e8cc46f40413963c1a5804b
chengheng10 <chengheng10@163.com>
2018-06-18 14:39:59 +0800

    Yztzy

--- src/main/java/YatzyGroup/DicePlayDoubles.java
+++ src/main/java/YatzyGroup/DicePlayDoubles.java
@@ -4,6 +4,20 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class DicePlayDoubles implements IDicePlayWays {
+    public int getPairs() {
+        return pairs;
+    }
+
+    public void setPairs(int pairs) {
+        this.pairs = pairs;
+    }
+
+    private int pairs =0;
+
+    public DicePlayDoubles() {
+        this.pairs = 2;
+    }
+
     public int getYatzyResult(int[] rolls) {
         int result = 0;
         HashMap<Integer,Integer> numberMap = new HashMap<Integer, Integer>();
@@ -17,8 +31,8 @@ public class DicePlayDoubles implements IDicePlayWays {
         }
         Set<Integer> numberSet = numberMap.keySet();
         for (Integer temp : numberSet) {
-            if (numberMap.get(temp) == 2) {
-                result = temp * 2;
+            if (numberMap.get(temp) == getPairs()) {
+                result = temp * getPairs();
             }
         }
         return result;
