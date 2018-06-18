3a217d1fbb4ed7284a686115971c23b37a742334
chengheng10 <chengheng10@163.com>
2018-06-18 16:20:52 +0800

    refact two pairs

--- src/main/java/YatzyGroup/DicePlayTwopairs.java
+++ src/main/java/YatzyGroup/DicePlayTwopairs.java
@@ -3,7 +3,7 @@ package YatzyGroup;
 import java.util.HashMap;
 import java.util.Set;
 
-public class DicePlayTwopairs implements IDicePlayWays {
+public class DicePlayTwopairs extends DicePlayDoubles implements IDicePlayWays {
     private int pairs = 0;
 
     public DicePlayTwopairs() {
@@ -21,16 +21,7 @@ public class DicePlayTwopairs implements IDicePlayWays {
     public int getYatzyResult(int[] rolls) {
         int result = 0;
         int count = 0;
-        HashMap<Integer, Integer> numberMap = new HashMap<Integer, Integer>();
-        for (int i = 0; i < rolls.length; i++) {
-            Integer tempNumber = new Integer(rolls[i]);
-            if (numberMap.containsKey(tempNumber)) {
-                numberMap.put(tempNumber, numberMap.get(tempNumber) + 1);
-            } else {
-                numberMap.put(tempNumber, 1);
-            }
-        }
-        Set<Integer> numberSet = numberMap.keySet();
+        super.initialResultMap(rolls);
         for (Integer temp : numberSet) {
             if (numberMap.get(temp) == getPairs()) {
                 count += 1;
